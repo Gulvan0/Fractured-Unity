6e8ce20d9d17d5a9025ef49e45c73e0bb1ea2a3d
Gulvan <mitmitmitmit@yandex.ru>
2019-07-07 16:40:38 +0300

    alpha 2.4

--- src/Main.hx
+++ src/Main.hx
@@ -53,7 +53,7 @@ class Main extends SSprite implements Listener
 	public static var ip(default, null):String = "ec2-18-224-7-170.us-east-2.compute.amazonaws.com";
 	#end
 
-	public static var version:String = "alpha2.3.1";
+	public static var version:String = "alpha2.4";
 	
 	public static var screenW(default, null):Int = 1366;
 	public static var screenH(default, null):Int = 768;
