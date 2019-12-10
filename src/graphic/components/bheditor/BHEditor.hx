6c151bd038ef5e28a0ac73ceceea8e5e2883a0d8
Gulvan <mitmitmitmit@yandex.ru>
2019-12-10 15:22:20 +0300

    Continuing to work on BHEditor

--- src/graphic/components/bheditor/BHEditor.hx
+++ src/graphic/components/bheditor/BHEditor.hx
@@ -1,5 +1,8 @@
 package graphic.components.bheditor;
 
+import openfl.geom.Rectangle;
+import openfl.display.JointStyle;
+import openfl.display.CapsStyle;
 import openfl.events.MouseEvent;
 import openfl.text.TextFormat;
 import openfl.display.MovieClip;
@@ -15,6 +18,8 @@ enum EditorMode
 
 class BHEditor extends SSprite
 {
+
+    private var BH_RECT:Rectangle = new Rectangle(0, 0, 750, 250);
 
     private var bg:ScalableBackground;
 
@@ -38,24 +43,81 @@ class BHEditor extends SSprite
     private var selectedPattern:Int;
     private var currentParams:Array<Array<Map<String, Float>>>;
 
-    private var onClosed:Void->Void;
+    private var onClosed:Void->Void; //Should return stringified pattern (use xmlutils or smth before returning). Use position of soul, fieldBorder and all the particles in container
 
     public function init(unfoldX:Float, unfoldY:Float)
     {
-        //add click, wheel, right-drag listeners
+        bg.x = unfoldX;
+        bg.y = unfoldY;
+        bg.unfoldAndInit(0.2);
+        addEventListener(MouseEvent.CLICK, clickHandler);
+        addEventListener(MouseEvent.MOUSE_DOWN, leftPressedHandler);
+        addEventListener(MouseEvent.MOUSE_UP, leftReleasedHandler);
+        addEventListener(MouseEvent.RIGHT_MOUSE_DOWN, rightPressedHandler);
+        addEventListener(MouseEvent.RIGHT_MOUSE_UP, rightReleasedHandler);
+        addEventListener(MouseEvent.MOUSE_WHEEL, wheelHandler);
     }
 
     private function deInit()
     {
         acceptBtn.removeEventListener(MouseEvent.CLICK, onAccept);
         declineBtn.removeEventListener(MouseEvent.CLICK, returnToParent);
-        //remove the listeners added in init()
+        removeEventListener(MouseEvent.CLICK, clickHandler);
+        removeEventListener(MouseEvent.MOUSE_DOWN, leftPressedHandler);
+        removeEventListener(MouseEvent.MOUSE_UP, leftReleasedHandler);
+        removeEventListener(MouseEvent.RIGHT_MOUSE_DOWN, rightPressedHandler);
+        removeEventListener(MouseEvent.RIGHT_MOUSE_UP, rightReleasedHandler);
+        removeEventListener(MouseEvent.MOUSE_WHEEL, wheelHandler);
+    }
+
+    private function rightPressedHandler(e:MouseEvent)
+    {
+        //startDrag for container of soul, fieldBorder and all the particles
+    }
+
+    private function rightReleasedHandler(e:MouseEvent)
+    {
+        //stopDrag for container of soul, fieldBorder and all the particles
+    }
+
+    private function leftPressedHandler(e:MouseEvent)
+    {
+        //if not addMode:
+        //add selection rect
+            addEventListener(MouseEvent.MOUSE_MOVE, moveHandler);
+    }
+
+    private function leftReleasedHandler(e:MouseEvent)
+    {
+        //if not addMode:
+            removeEventListener(MouseEvent.MOUSE_MOVE, moveHandler);
+        //push all the intersected particles into "selected" array (use selrect.x,.y and e.localX,.localY) | use select(x1,x2,y1,y2) function that will _/reinit parambox/delete selected
+        //remove selection rect
+    }
+
+    private function moveHandler(e:MouseEvent)
+    {
+        //reshape selection rect 
+    }
+
+    private function clickHandler(e:MouseEvent)
+    {
+        //switch mode - add (scale if needed before adding to container, mind the coord system), select or delete
+    }
+
+    private function wheelHandler(e:MouseEvent)
+    {
+        //Depending on the e.delta's sign:
+        //if scale (scaleX for example or use additional property like in bg) hasn't reached its upper/lower limit:
+        //increase/decrease scaleX, scaleY of container of soul, fieldBorder and all the particles
+        //zoom/zoomout bg (remove the wheel event handling from the bg's code)
     }
 
     private function onAccept(e)
     {
         //send change request, wait till the answer is recieved, then:
         //returnToParent();
+        //Keep in mind that user should not be able to send multiple requests
     }
 
     private function returnToParent(?e)
@@ -98,13 +160,31 @@ class BHEditor extends SSprite
         textPatterns.text = "Patterns";
         paramBox = new ParamBox();
         fieldBorder = new SSprite();
+        fieldBorder.graphics.lineStyle(5, 0xffffff, 1, false, null, CapsStyle.SQUARE, JointStyle.MITER);
+        fieldBorder.graphics.drawRect(0, 0, BH_RECT.width, BH_RECT.height);
         soul = Assets.getSoul();
-        particles = [];
+        particles = [];//fill according to pattern, add nulls
         mode = EditorMode.Add;
         this.selectedPattern = selectedPattern;
         currentParams = [];//fill according to patterns
-        acceptBtn.addEventListener(MouseEvent.CLICK, onAccept);//configure everything created above this line
+        //container = new SSprite();
+        acceptBtn.addEventListener(MouseEvent.CLICK, onAccept);
         declineBtn.addEventListener(MouseEvent.CLICK, returnToParent);
-        //add everything on stage
+
+        add(bg, 0, 0);
+        //particles, border and soul should be added to container!
+        //add particles according to selectedPattern
+        add(fieldBorder, 0, 0);
+        add(soul, fieldBorder.x + soul.width * 2, fieldBorder.y + BH_RECT.height / 2);
+        //add(container, 330, 255);
+        for (i in 0...3)
+            add(patternBtns[i], 45 + 60 * i, 68);
+        add(acceptBtn, 1240, 35);
+        add(declineBtn, 1240, 35);
+        add(addBtn, 40, 135);
+        add(editBtn, 40, 202);
+        add(deleteBtn, 40, 269);
+        add(textPatterns, 40, 30);
+        add(paramBox, 25, 340);
     }
 }
